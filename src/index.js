911e644a8220ad93dc4c48fe4b47912e023a49c7
zhouhuafei <zhouhuafei@shopex.cn>
2018-03-13 13:49:50 +0800

    min

--- src/index.js
+++ src/index.js
@@ -10,15 +10,45 @@
         window.zhf[name] = factory();
     }
 })('getDomArray', function () {
-    function getDomArray(element) {
+    /**
+     *  @param element 第一参数
+     *  @param parentElement 第二参数 - 只有第一参数是字符串的情况下才会有效
+     */
+    function getDomArray(element, parentElement) {
         let dom = [];
+        let parentDom = document;
+        if (parentElement) {
+            // 如果是字符串
+            if (Object.prototype.toString.call(parentElement).slice(8, -1).toLowerCase() === 'string') {
+                parentDom = document.querySelector(parentElement);
+            }
+            // 如果是dom节点(一个元素)    原生的
+            if (parentElement.nodeType === 1) {
+                parentDom = parentElement;
+            }
+            // 如果是document
+            if (parentElement === document) {
+                parentDom = parentElement;
+            }
+            /*
+             * 如果是dom集合(一组元素)    HtmlCollection(通过getElementsBy系列获取到的)
+             * 如果是dom集合(一组元素)    NodeList(通过querySelectorAll获取到的)
+             * */
+            if (Object.prototype.toString.call(parentElement).slice(8, -1).toLowerCase() === 'htmlcollection' || Object.prototype.toString.call(parentElement).slice(8, -1).toLowerCase() === 'nodelist') {
+                parentDom = [].slice.call(parentElement)[0];
+            }
+        }
         if (element) {
             // 如果是字符串
             if (Object.prototype.toString.call(element).slice(8, -1).toLowerCase() === 'string') {
-                dom = [].slice.call(document.querySelectorAll(element));
+                dom = [].slice.call(parentDom.querySelectorAll(element));
             }
             // 如果是dom节点(一个元素)    原生的
             if (element.nodeType === 1) {
+                dom = [element];
+            }
+            // 如果是document
+            if (element === document) {
                 dom = [element];
             }
             /*
